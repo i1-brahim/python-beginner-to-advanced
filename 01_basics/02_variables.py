950de85c70140f8641604d059d6fb616a7e50b98
i1-brahim <i10357986@gmail.com>
2026-02-07 23:18:17 +0300

    Add variables basics
    Learned Python variable fundamentals:
    - variable assignment
    - basic data types
    - type() usage
    Practice file included.

---
+++ 01_basics/02_variables.py
@@ -0,0 +1,20 @@
+# 02_variables.py
+# In this file, I'm learning about the concept of variables in Python.
+# Note: These are my actual data as of February 7, 2026.
+Name = "İbrahim"
+Age = 15
+Height = 1.73
+Weight = 59
+Is_Student = True
+
+print(Name)
+print(Age)
+print(Height)
+print(Weight)
+print(Is_Student)
+
+print(type(Name))
+print(type(Age))
+print(type(Height))
+print(type(Weight))
+print(type(Is_Student))
---
+++ 01_basics/practice_variables.py
@@ -0,0 +1,20 @@
+His_Name = "İbrahim"
+His_Wants = "Be a succesful person and be a nice life"
+His_Goals = "To believe and not to despair "
+His_IQ_Average = 100.5
+His_Favorite_Number = 13
+His_Desktop_Computer_Is_Perfect = False
+
+print(His_Name)
+print(His_Wants)
+print(His_Goals)
+print(His_IQ_Average)
+print(His_Favorite_Number)
+print(His_Desktop_Computer_Is_Perfect)
+
+print(type(His_Name))
+print(type(His_Wants))
+print(type(His_Goals))
+print(type(His_IQ_Average))
+print(type(His_Favorite_Number))
+print(type(His_Desktop_Computer_Is_Perfect))
