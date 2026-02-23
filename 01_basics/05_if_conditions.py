7bc64a24d532f70416ac46d3d04aa4a3403e894c
i1-brahim <i10357986@gmail.com>
2026-02-23 12:59:46 +0300

    Add basic if-else number classification
    Implemented a basic if/elif/else structure to classify a number as positive, negative, or zero.
    This version focuses purely on conditional logic without input validation.

---
+++ 01_basics/05_if_conditions.py
@@ -0,0 +1,11 @@
+number = float(input("Enter A Number: "))
+if number > 0:
+    result = "This Number Is Positive"
+
+elif number < 0:
+    result = "This Number Is Negative"
+
+else:
+    result = "This Is Zero"
+
+print(result)
